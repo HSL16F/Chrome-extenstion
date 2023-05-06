5b79f5045872d89c41265fcf263fea4388aeb69c
HarrisonL <Harrison.Langdon@outlook.com>
2023-05-06 11:44:11 +1000

    Developing backend framework

---
+++ Backend/flask.py
@@ -0,0 +1,16 @@
+# app.py
+from flask import Flask, request
+
+app = Flask(__name__)
+
+@app.route('/process-text', methods=['POST'])
+def process_text():
+    # Parse the JSON data sent from the browser
+    data = request.get_json()
+
+    # Process the text data
+    text = data['text']
+    processed_text = text.upper()
+
+    # Return the processed text as a JSON response
+    return {'processed_text': processed_text}
